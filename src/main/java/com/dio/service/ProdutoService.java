89ea65d942158a7e8ea08e5d002680036737c9be
MateusAntony <mateus.medeiros110@gmail.com>
2025-02-18 16:45:30 -0300

    first commit

---
+++ src/main/java/com/dio/service/ProdutoService.java
@@ -0,0 +1,30 @@
+package com.dio.service;
+
+
+import com.dio.entity.Produto;
+import com.dio.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repository;
+
+    public Produto save(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public Produto findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Produto> findAll() {
+        return repository.findAll();
+    }
+
+
+}
